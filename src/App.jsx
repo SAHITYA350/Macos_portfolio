5994aa322c2237014cfdf89b47159ebe741b1192
Sahitya Ghosh <sahityaghosh350@gmail.com>
2025-12-15 13:46:36 +0530

    My macos type portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from '#components/Navbar'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-2xl">
+      <main>
+        <Navbar />
+      </main>
+    </div>
+  )
+}
+
+export default App
